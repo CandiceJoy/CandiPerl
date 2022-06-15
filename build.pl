ec58853d0cd1e495b06052a1d22cd99a7fe2e71d
CandiceJoy <candice@candicejoy.com>
2022-06-14 19:47:58 -0500

    fix

--- build.pl
+++ build.pl
@@ -1,10 +1,10 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use strict;
 use warnings;
 
 use ExtUtils::MakeMaker;
 
 WriteMakefile(
-    NAME            => 'Your::Module',
+    NAME            => 'Candi::CandiLib',
     VERSION_FROM    => 'lib/CandiLib/CandiLib.pm'
 );
