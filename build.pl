fde9057fb8e63b51fc803cf43145cfa93435404a
CandiceJoy <candice@candicejoy.com>
2022-06-14 19:16:59 -0500

    fix

--- build.pl
+++ build.pl
@@ -1,27 +1,10 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Module::Build;
 
-use 5.008;
+use ExtUtils::MakeMaker;
 
-my $builder = Module::Build->new(
-    module_name        => "CandiLib",
-    license            => "MIT",
-    dist_author        => "CandiceJoy",
-    dist_abstract      => "Candi Perl Lib",
-    create_makefile_pl => 0,
-    script_files       => 'script/app.pl',
-    create_readme      => 0,
-    requires           => { "Data::Dumper" => "0" },
-    build_requires     => { "Data::Dumper" => "0" },
-    meta_merge         => {
-        resources => {
-            repository => 'https://github.com/CandiceJoy/CandiPerl',
-            bugtracker => 'https://github.com/szabgab/CandiPerl/issues'
-        }
-    },
-
+WriteMakefile(
+    NAME            => 'Your::Module',
+    VERSION_FROM    => 'lib/CandiLib/CandiLib.pm'
 );
-
-$builder->create_build_script();
